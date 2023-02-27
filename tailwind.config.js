f58b95880c57893af0e7b7a6fda2eec145039543
Valentin Cassarino <valencassa@gmail.com>
2023-02-27 19:25:57 -0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["var(--satoshi-font)", ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
